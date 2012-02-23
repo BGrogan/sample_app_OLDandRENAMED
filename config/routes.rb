648b0c5f53fc0da6ae530901d8bd789e514d5e96
Brian Grogan <groganb15@uww.edu>
2012-02-23 02:49:45 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 SampleApp::Application.routes.draw do
+get "users/new"
 
-	match '/contact', :to => 'pages#contact'
-	match '/about', :to => 'pages#about'
-	match '/help', :to => 'pages#help'
+match '/signup', :to => 'users#new'
+match '/contact', :to => 'pages#contact'
+match '/about', :to => 'pages#about'
+match '/help', :to => 'pages#help'
+root :to => 'pages#home'
 
-  get "pages/home"
-
-  get "pages/contact"
-
+	get "pages/home"
+  	get "pages/contact"
 	get "pages/about"
 	
-	SampleApp::Application.routes.draw do
-		match '/contact', :to => 'pages#contact'
-		match '/about', :to => 'pages#about'
-		match '/help', :to => 'pages#help'
-		root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
